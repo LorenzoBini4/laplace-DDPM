fb9539f7f740f50311e289de34352b7101e44cff
Lorenzo Bini <lorenzo.bini@unige.ch>
2025-05-16 09:19:26 +0200

    slurm

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 # python -u lap_ddpm.py > run_lap.out 2> run_lap.err
 # python -u cond_lap_ddpm.py > run_cond_lap.o 2> run_cond_lap.e
 # python -u adv_cond_lap_ddpm.py > run_adv_cond_lap.o 2> run_adv_cond_lap.e
-python -u adv_delme.py > run_delme.o 2> run_delme.e
+python -u delme.py > run_delme.o 2> run_delme.e
