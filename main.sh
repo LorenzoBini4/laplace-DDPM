d69f73fafb8189c03613cf5806a0c66316f37e23
Lorenzo Bini <lorenzo.bini@unige.ch>
2025-06-16 16:44:37 +0200

    upd sh

--- main.sh
+++ main.sh
@@ -1,2 +1,2 @@
-python -u main_pbmc3k.py > run_ddpm_main.out 2> run_ddpm_main.err
-# python -u main_denta.py > run_ddpm_main.out 2> run_ddpm_main.err
+python -u run_pbmc3k.py > run_ddpm_main.out 2> run_ddpm_main.err
+# python -u run_denta.py > run_ddpm_main.out 2> run_ddpm_main.err
